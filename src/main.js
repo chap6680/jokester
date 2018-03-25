65226df79154dd152b09fae910e4734c5cde4052
david chapman <chap6680@gmail.com>
2018-03-25 01:15:39 -0600

    final with files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './components/App.vue'
 import store from './store'
 
-store.commit('INIT_JOKES', [{ test: 'test J' }, { test2: 'testJ2' }])
+//store.commit('INIT_JOKES', [{ test: 'test J' }, { test2: 'testJ2' }])
 
 new Vue({
 	el: '#app',
